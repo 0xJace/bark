ab5422f2c42e63ee8bc09ff9bffcc0dc395ca4a9
0xJace <134779144+0xJace@users.noreply.github.com>
2023-07-01 23:56:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,50 @@
 # Instructions for starting runpod container here
+echo "Container is running"
+
+# Sync venv to workspace to support Network volumes
+echo "Syncing venv to workspace, please wait..."
+rsync -au /venv/ /workspace/venv/
+rm -rf /venv
+
+# Sync Web UI to workspace to support Network volumes
+echo "Syncing Bark Web UI to workspace, please wait..."
+rsync -au /bark/ /workspace/bark/
+rm -rf /bark
+
+# Fix the venvs to make them work from /workspace
+echo "Fixing Stable Diffusion Web UI venv..."
+/fix_venv.sh /venv /workspace/venv
+
+if [[ ${PUBLIC_KEY} ]]
+then
+    echo "Installing SSH public key"
+    mkdir -p ~/.ssh
+    echo ${PUBLIC_KEY} >> ~/.ssh/authorized_keys
+    chmod 700 -R ~/.ssh
+    service ssh start
+    echo "SSH Service Started"
+fi
+
+if [[ ${JUPYTER_PASSWORD} ]]
+then
+    echo "Starting Jupyter lab"
+    ln -sf /examples /workspace
+    ln -sf /root/welcome.ipynb /workspace
+
+    source /workspace/venv/bin/activate
+    cd /
+    nohup jupyter lab --allow-root \
+        --no-browser \
+        --port=8888 \
+        --ip=* \
+        --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' \
+        --ServerApp.token=${JUPYTER_PASSWORD} \
+        --ServerApp.allow_origin=* \
+        --ServerApp.preferred_dir=/workspace &
+    echo "Jupyter Lab Started"
+    deactivate
+fi
+
+echo "All services have been started"
+
+sleep infinity
